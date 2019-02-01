2fa99f602028a4ece648ea7c17521695f115fb26
Alexandre PEINOT <alexandre.peinot@eyesnap.fr>
2019-02-01 08:55:53 +0100

    Add basic test case and package entry in main files
    Package CIserver
    Test assert(true, true)

---
+++ src/test/java/ServerTest.java
@@ -0,0 +1,14 @@
+//Test cases of the CI server
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ServerTest {
+
+    @Test
+    public void testTrue() {
+        assertEquals(true, true);
+    }
+
+}
