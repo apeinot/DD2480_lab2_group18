6ac75f436b24f2fea70bf6ba39a7790395c4a3bb
Emil Elmarsson <emilelm@kth.se>
2019-02-01 12:33:56 +0100

    Added dependencies needed for sending e-mail #2
    Added the JavaMail API and Java Activation Framework (JAF) to the build file, which are used for sending e-mail. Also created a Responder class, which I plan to add the e-mail sending code in. Possibly this class could also contain the git responder, which Franz will develop.

---
+++ src/main/java/Responder.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
+
+public class Responder{
+    public static void git(){
+
+    }
+
+    public static void email(){
+
+    }
+}
