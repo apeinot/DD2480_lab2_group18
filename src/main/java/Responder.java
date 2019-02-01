c4af55e060448c850637b1c354233971673851f5
Emil Elmarsson <emilelm@kth.se>
2019-02-01 13:32:22 +0100

    Added e-mail function for sending e-mails #2
    The function has void return, takes four parameters; from (the e-mail address of the sender), to (the e-mail address of the recipient), subject (the title/subject of the e-mail) and text (the content of the e-mail). These might be subject to changes, as I have not figured out what host to use for the e-mails yet.

--- src/main/java/Responder.java
+++ src/main/java/Responder.java
@@ -1,3 +1,5 @@
+package CIserver;
+
 import java.util.*;
 import javax.mail.*;
 import javax.mail.internet.*;
@@ -8,7 +10,38 @@ public class Responder{
 
     }
 
-    public static void email(){
+    /**
+    Sends an e-mail.
+    @param from - E-mail address of sender.
+    @param to - E-mail address of recipient.
+    @param subject - The title (subject) of the e-mail.
+    @param text - The e-mail content.
+    */
+    public static void email(String from, String to, String subject, String text){
+        String host = "localhost";
 
+        Properties properties = System.getProperties();
+        properties.setProperty("mail.smtp.host", host);
+
+        Session session = Session.getDefaultInstance(properties);
+        try{
+            // Create e-mail.
+            MimeMessage message = new MimeMessage(session);
+
+            // Set sender
+            message.setFrom(new InternetAddress(from));
+            // Set recipient
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            // The subject of the e-mail
+            message.setSubject(subject);
+            // The actual e-mail content
+            message.setText(text);
+
+            // Send e-mail
+            Transport.send(message);
+        }catch(MessagingException e){
+            e.printStackTrace();
+        }
     }
 }
