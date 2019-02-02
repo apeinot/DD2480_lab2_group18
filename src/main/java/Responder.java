ed3ecd61b224da27b47469b52772f9b4a0740cb2
emilelmarsson <emil.elmarsson@gmail.com>
2019-02-02 19:46:23 +0100

    Now sending e-mails through Google's SMTP server. Also added tests. #2
    The Gmail account used is mailbot8080@gmail.com with password "bot12345". Included the JavaMail and Java Activation Framework in the testing classpath, since I forgot to do that last time.

--- src/main/java/Responder.java
+++ src/main/java/Responder.java
@@ -11,19 +11,41 @@ public class Responder{
     }
 
     /**
-    Sends an e-mail.
-    @param from - E-mail address of sender.
+    Sends an e-mail from our mailbot.
     @param to - E-mail address of recipient.
     @param subject - The title (subject) of the e-mail.
     @param text - The e-mail content.
+    @return - Returns 0 if an e-mail was sent successfully, 1 if there was an
+              authentication error or some other error when creating the e-mail.
     */
-    public static void email(String from, String to, String subject, String text){
-        String host = "localhost";
+    public static int send(final String to, final String subject, final String text){
+        return send("mailbot8080@gmail.com", "bot12345", to, subject, text);
+    }
 
-        Properties properties = System.getProperties();
-        properties.setProperty("mail.smtp.host", host);
+    /**
+    Sends an e-mail through Gmail's SMTP server. The sender needs to allow less
+    secure apps to access their account.
+    @param from - E-mail address of the sender.
+    @param password - E-mail password of the sender.
+    @param to - E-mail address of recipient.
+    @param subject - The title (subject) of the e-mail.
+    @param text - The e-mail content.
+    @return - Returns 0 if an e-mail was sent successfully, 1 if there was an
+              authentication error or some other error when creating the e-mail.
+    */
+    public static int send(final String from, final String password, final String to, final String subject, final String text){
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "587");
 
-        Session session = Session.getDefaultInstance(properties);
+        // Authenticating the mail session
+        Session session = Session.getInstance(props, new javax.mail.Authenticator(){
+            protected PasswordAuthentication getPasswordAuthentication(){
+                return new PasswordAuthentication(from, password);
+            }
+        });
         try{
             // Create e-mail.
             MimeMessage message = new MimeMessage(session);
@@ -32,7 +54,6 @@ public class Responder{
             message.setFrom(new InternetAddress(from));
             // Set recipient
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
             // The subject of the e-mail
             message.setSubject(subject);
             // The actual e-mail content
@@ -41,7 +62,9 @@ public class Responder{
             // Send e-mail
             Transport.send(message);
         }catch(MessagingException e){
-            e.printStackTrace();
+            //e.printStackTrace();
+            return 1;
         }
+        return 0;
     }
 }
--- src/test/java/ServerTest.java
+++ src/test/java/ServerTest.java
@@ -11,4 +11,15 @@ public class ServerTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void emailTest(){
+        // The mail bot e-mails itself, should work and return 0.
+        assertEquals(CIserver.Responder.send("mailbot8080@gmail.com", "Hello", "there"), 0);
+        // Entering the wrong password for the bot, should not work (return code 1).
+        assertEquals(CIserver.Responder.send("mailbot8080@gmail.com", "password", "mailbot8080@gmail.com", "Hello", "there"), 1);
+        // Nonsensical sender address, should not work (return code 1).
+        assertEquals(CIserver.Responder.send("sfjsfjtset", "sfsdfsdf", "mailbot8080@gmail.com", "Hello", "there"), 1);
+        // Nonsensical recipient address, should not work (return code 1).
+        assertEquals(CIserver.Responder.send("hlejsdjfosfkke", "Hello", "there"), 1);
+    }
 }
