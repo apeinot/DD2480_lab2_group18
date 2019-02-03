50b87919332413771fc29b51e7b56c6460e3322f
Alexandre PEINOT <alexandre.peinot@eyesnap.fr>
2019-02-03 10:46:23 +0100

    Fix for #4: add the possibility to run a command in a different directory
    Add 2nd version of execute function and test

--- src/test/java/ServerTest.java
+++ src/test/java/ServerTest.java
@@ -18,6 +18,8 @@ public class ServerTest {
     First test - try to execute the command 'ls' which should work and return 0 as exit code
     Second test - try to execute the command 'ls monkey' which should result 
                   in an error (the monkey directory doesn't exist) and return 2 as exit code
+    Third test - try to execute the command 'ls' in the directory 'src/' which should work
+                 and return 0 as exit code
     (The output of both commands is stored in 'history/test/test.txt')
     */
     @Test
@@ -40,6 +42,12 @@ public class ServerTest {
 	command = "ls monkey";
 	exitCode = CIserver.Execution.execute(command, file);
         assertEquals(exitCode, 2);
+
+	//Test 3
+	File dir = new File("src/");
+        command = "ls";
+        exitCode = CIserver.Execution.execute(command, file, dir);
+        assertEquals(exitCode, 0);
     }
 
 }
