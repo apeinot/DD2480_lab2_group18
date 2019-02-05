1bdaa31616663a23f79f2bcf7bbdd49c77580299
Samuel <shert@kth.se>
2019-02-05 20:11:42 +0100

    Tests for field extraction/storage method
    One test will all bad data. Tests robustness.
    
    One test with partially bad data. Tests robustness and partial data 
    read.
    
    One test with all good data. Test complete data read

--- src/test/java/RequestParserTest.java
+++ src/test/java/RequestParserTest.java
@@ -1,5 +1,3 @@
-//Test cases of the CI server
-
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
@@ -18,23 +16,38 @@ public class RequestParserTest{
     */
     @Test
     public void getFiedsTest(){
-
+        RequestParser rp = new RequestParser();
+        
         //Testing the field getter when fields are non existent, should:
         //1) Not crash (test of try catch) and
         //2) Not set the values.
-        RequestParser rp = new RequestParser();
         String jsonString = "{\"nothing\":\"of\", \"use\": \"test\"}";
         JSONObject jo = new JSONObject(jsonString);
         rp.getFields(jo);
         assertEquals(rp.getAfterField(), null);
         assertEquals(rp.getStatuses_urlField(), null);
 
-        //Testing the field getter when the fields exist, should set both fields correctly
-        jsonString = "{\"after\":\"correct_string\",\"repository\":{\"statuses_url\":\"Correct.url/test\"}}";
+        //Testing the field getter when the fields exist, should:
+        //1) Not crash and
+        //2) Set all values correctly
+        jsonString = "{\"after\":\"correct_string\",\"repository\":{\"statuses_url\":\"Correct.url/test\",\"clone_url\":\"Correct.url/clone/test\",\"name\":\"correct_name\"}}";
         jo = new JSONObject(jsonString);
         rp.getFields(jo);
         assertEquals(rp.getAfterField(), "correct_string");
         assertEquals(rp.getStatuses_urlField(), "Correct.url/test");
+        assertEquals(rp.getClone_urlField(), "Correct.url/clone/test");
+        assertEquals(rp.getNameField(), "correct_name");
+
+        //Testing the field getter when some fields exist, should:
+        //1) Not crash and
+        //2) Set the 'after' & name values correctly
+        jsonString = "{\"after\":\"correct_string\",\"repository\":{\"name\":\"correct_name\"}}";
+        jo = new JSONObject(jsonString);
+        rp.getFields(jo);
+        assertEquals(rp.getAfterField(), "correct_string");
+        assertEquals(rp.getStatuses_urlField(), null);
+        assertEquals(rp.getClone_urlField(), null);
+        assertEquals(rp.getNameField(), "correct_name");
     }
 
 }
