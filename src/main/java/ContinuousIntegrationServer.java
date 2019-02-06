8bbc5f168912e41e2ed1f9069f3ca942b497014e
Jonathan Broström <jonis555@yahoo.se>
2019-02-06 18:41:05 +0100

    Added support for distinguishing between POST and GET
    The handle method now recognizes whether it is handling a POST or GET request, to enable future access to logfiles.
    This is part of #5

--- src/main/java/ContinuousIntegrationServer.java
+++ src/main/java/ContinuousIntegrationServer.java
@@ -4,10 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import java.io.File;
-import java.io.FileReader;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.lang.StringBuilder;
+import java.io.PrintWriter;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
@@ -30,105 +28,110 @@ public class ContinuousIntegrationServer extends AbstractHandler
         baseRequest.setHandled(true);
 
         System.out.println(target);
-
+        String method = request.getMethod();
+        PrintWriter out = response.getWriter();
         // here you do all the continuous integration tasks
         // for example
         // 1st clone your repository
         // 2nd compile the code
-        RequestParser parser = new RequestParser();
-        int returncode;
-        if ((returncode = parser.parse(request)) != 1) {
+        if (method.equals("GET")) {
 
-        }
-        String commit_id = parser.getAfterField();
-        String clone_url = parser.getClone_urlField();
-        String reponame = parser.getNameField();
-        String name = clone_url.split("/")[3];
-        Responder resp = new Responder();
-        String[] request_params = new String[4];
-        request_params[0] = "pending";
-        request_params[1] = "http://130.237.227.78:8018";
-        request_params[2] = "Waiting for results from CI server";
-        request_params[3] = "CI Server of Group 18";
-        String url_string = "https://api.github.com/repos/"+name+"/"+reponame+"/statuses/";
-        if (resp.git_status(url_string, commit_id, request_params) != 0) {
+        } else if (method.equals("POST")) {
+            RequestParser parser = new RequestParser();
+            int returncode;
+            if ((returncode = parser.parse(request)) != 1) {
 
-        }
-
-        File file = new File("history/"+name +"/"+reponame+"/"+commit_id+".log");
-        try {
-            if(file.getParentFile().mkdirs()) {
-                file.createNewFile();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        File dir = new File(reponame+"/");
-        // Run CI tests
-        boolean[] commandfail = new boolean[3];
-        String result = "success";
-        if ((returncode = Execution.execute("git clone " + clone_url, file)) != 0) {
-            // Something went wrong
-        }
-        if ((returncode = Execution.execute("git checkout "+commit_id, file, dir)) != 0){
+            String commit_id = parser.getAfterField();
+            String clone_url = parser.getClone_urlField();
+            String reponame = parser.getNameField();
+            String name = clone_url.split("/")[3];
+            Responder resp = new Responder();
+            String[] request_params = new String[4];
+            request_params[0] = "pending";
+            request_params[1] = "http://130.237.227.78:8018";
+            request_params[2] = "Waiting for results from CI server";
+            request_params[3] = "CI Server of Group 18";
+            String url_string = "https://api.github.com/repos/"+name+"/"+reponame+"/statuses/";
+            if (resp.git_status(url_string, commit_id, request_params) != 0) {
 
-        }
-        if ((returncode = Execution.execute("ant compile", file, dir)) != 0){
-            commandfail[0] = true;
-            result = "failure";
-        }
-        if ((returncode = Execution.execute("ant test-compile", file, dir)) != 0){
-            commandfail[1] = true;
-            result = "failure";
-        }
-        if ((returncode = Execution.execute("ant test", file, dir)) != 0){
-            commandfail[2] = true;
-            result = "failure";
-        }
-        if ((returncode = Execution.execute("rm -rf " + reponame, file)) != 0){
-
-        }
-
-        //Set github status
-
-        String desc = "All tests completed successfully.";
-        if (commandfail[0]) {
-            if (commandfail[1]) {
-                desc = "Program and test compilation failed.";
-            } else {
-                desc = "Program compilation failed";
             }
-        } else if (commandfail[1]) {
-            desc = "Test compilation failed";
-        } else if (commandfail[2]) {
-            desc = "One or more tests failed";
-        }
-        request_params[0] = result;
-        request_params[2] = desc;
-        if (resp.git_status(url_string, commit_id, request_params) != 0){
-            //Status set successfully
-        }
 
-        //Send email
-        String subject = "Build result of commit "+commit_id;
-        String mailText = "All tests ran successfully. "+request_params[1];
-        if (commandfail[0]) {
-            if (commandfail[1]) {
-                mailText = "Program and test compilation failed.\n"
+            File file = new File("history/"+name +"/"+reponame+"/"+commit_id+".log");
+            try {
+                if(file.getParentFile().mkdirs()) {
+                    file.createNewFile();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            File dir = new File(reponame+"/");
+            // Run CI tests
+            boolean[] commandfail = new boolean[3];
+            String result = "success";
+            if ((returncode = Execution.execute("git clone " + clone_url, file)) != 0) {
+                // Something went wrong
+            }
+            if ((returncode = Execution.execute("git checkout "+commit_id, file, dir)) != 0){
+
+            }
+            if ((returncode = Execution.execute("ant compile", file, dir)) != 0){
+                commandfail[0] = true;
+                result = "failure";
+            }
+            if ((returncode = Execution.execute("ant test-compile", file, dir)) != 0){
+                commandfail[1] = true;
+                result = "failure";
+            }
+            if ((returncode = Execution.execute("ant test", file, dir)) != 0){
+                commandfail[2] = true;
+                result = "failure";
+            }
+            if ((returncode = Execution.execute("rm -rf " + reponame, file)) != 0){
+
+            }
+
+            //Set github status
+
+            String desc = "All tests completed successfully.";
+            if (commandfail[0]) {
+                if (commandfail[1]) {
+                    desc = "Program and test compilation failed.";
+                } else {
+                    desc = "Program compilation failed";
+                }
+            } else if (commandfail[1]) {
+                desc = "Test compilation failed";
+            } else if (commandfail[2]) {
+                desc = "One or more tests failed";
+            }
+            request_params[0] = result;
+            request_params[2] = desc;
+            if (resp.git_status(url_string, commit_id, request_params) != 0){
+                //Status set successfully
+            }
+
+            //Send email
+            String subject = "Build result of commit "+commit_id;
+            String mailText = "All tests ran successfully. "+request_params[1];
+            if (commandfail[0]) {
+                if (commandfail[1]) {
+                    mailText = "Program and test compilation failed.\n"
+                            +request_params[1]+"\n\nLog:\n"+file.toString();
+                } else {
+                    mailText = "Program compilation failed.\n"
+                            +request_params[1]+"\n\nLog:\n"+file.toString();
+                }
+            } else if (commandfail[1]) {
+                mailText = "Test compilation failed.\n"
                         +request_params[1]+"\n\nLog:\n"+file.toString();
-            } else {
-                mailText = "Program compilation failed.\n"
+            } else if (commandfail[2]) {
+                mailText = "One or more tests failed.\n"
                         +request_params[1]+"\n\nLog:\n"+file.toString();
             }
-        } else if (commandfail[1]) {
-            mailText = "Test compilation failed.\n"
-                    +request_params[1]+"\n\nLog:\n"+file.toString();
-        } else if (commandfail[2]) {
-            mailText = "One or more tests failed.\n"
-                    +request_params[1]+"\n\nLog:\n"+file.toString();
-        }
-        if (resp.send("mailbot8080@gmail.com", subject, mailText) != 0) {
-            //E-mail sent successfully
+            if (resp.send("mailbot8080@gmail.com", subject, mailText) != 0) {
+                //E-mail sent successfully
+            }
         }
 
         response.getWriter().println("CI job done");
