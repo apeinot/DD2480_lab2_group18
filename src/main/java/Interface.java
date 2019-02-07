9ac043785c42a17b5fac4e3a13e25f24b538507a
emilelmarsson <emil.elmarsson@gmail.com>
2019-02-07 13:15:39 +0100

    Added documentation for the Interface functions. #22
    Commented and documented the readFile() and get() functions.

--- src/main/java/Interface.java
+++ src/main/java/Interface.java
@@ -11,7 +11,11 @@ public class Interface{
     public static final String DOMAIN = "http://130.237.227.78:8018/";
 
     /**
-
+    Returns the contents of a file in a string.
+    @param file - A file.
+    @return a string that contains the contents of the file, or if there is a
+            FileNotFoundException, NullPointerException or another exception,
+            return null.
     */
     public static String readFile(File file){
         try{
@@ -28,23 +32,33 @@ public class Interface{
     }
 
     /**
-    Måste skrivas.
-    @param request -
-    @return 0 if all went well, anything else means something went wrong.
+    Used for reading log files and creating the HTML that is to be rendered
+    on the web interface. If the current path is a directory, output all of the
+    links to all the subdirectories and files, as well as a link for getting back
+    to the previous directory. If the current path is a file, output its contents.
+    @param path - the (relative) path of the current directory.
+    @return a string containing an HTML document that is to be put on the web
+            interface, or if there were any errors null is returned.
     */
     public static String get(String path){
+        // The HTML template file that is to be filled with a body and a title.
         String template = readFile(new File("src/main/java/template.html"));
+        // Template file wasn't found.
         if(template == null){
             return null;
         }
         try{
             File file = new File(path);
+            // If the current file or directory exists
             if(file.exists()){
                 String title = file.getName();
                 String body = "";
+                // If it's a file
                 if(file.isFile()){
                     body = readFile(file);
-                }else if(file.isDirectory()){
+                }
+                // If it's a directory.
+                else if(file.isDirectory()){
                     String[] parts = path.split("/");
                     String newPath = "";
                     for(int i=0; i<parts.length - 1; i++){
@@ -55,6 +69,7 @@ public class Interface{
                         body += "<a href=" + DOMAIN + path + entry.getName() + ">" + entry.getName() + "</a><br>";
                     }
                 }
+                // Add title and body to HTML document.
                 template = template.replace("$title", title);
                 template = template.replace("$body", body);
             }else{
