6c249c885853f6f5c8097a37e0049b3debb3bc55
apeinot <35868146+apeinot@users.noreply.github.com>
2019-02-07 15:11:08 +0100

    Update ServerTest.java

--- src/test/java/ServerTest.java
+++ src/test/java/ServerTest.java
@@ -11,7 +11,7 @@ public class ServerTest {
 
     @Test
     public void testTrue() {
-        assertEquals(true, false);
+        assertEquals(true, true);
     }
 
     /**Test cases of the execute method from the class Execution
